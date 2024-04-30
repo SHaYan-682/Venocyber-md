e02a9722ec7a07dc6965c3ac17a435efd5c3b38b
SHaYan-682 <168551593+SHaYan-682@users.noreply.github.com>
2024-04-30 18:34:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.blockJids= process.env.BLOCK_JID || "120363023983262391@g.us"
 
 global.DATABASE_URI = process.env.DATABASE_URL || ""
 
-global.timezone= process.env.TZ || process.env.TIME_ZONE || "Africa/Dodoma";
+global.timezone= process.env.TZ || process.env.TIME_ZONE || "Khyber/Jamrud";
 global.github=process.env.GITHUB|| "https://github.com/QXS-871";
 global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/KSPfEzy2fpcAZT89AifEmb";
 global.website=process.env.GURL || "https://chat.whatsapp.com/KSPfEzy2fpcAZT89AifEmb" ; 
